fbc67717149cc13fb9c33dbe24ae086d2662c491
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2021-06-23 01:19:11 -0300

    [Mission_ ReactJS] Aula 02 – Maximum Speed

---
+++ src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { useHistory } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import logoGoogle from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button';
+
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    } 
+    history.push('/rooms/new');
+  }
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Illustration" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+        <button className="create-room" onClick={handleCreateRoom}>
+          <img src={logoGoogle} alt="Logo Google" />
+          Crie sua sala com o Google
+        </button>
+        <div className="separator">ou entre em uma sala</div>
+        <form>
+          <input 
+            type="text"
+            placeholder="Digite o código da sala"
+          />
+          <Button type="submit">Entrar na sala </Button>
+        </form>
+        </div>
+      </main>
+    </div>
+  )
+}
